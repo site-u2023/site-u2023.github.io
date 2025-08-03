af37600154e413834e4dd4258363454a859ea666
site_u <140032047+site-u2023@users.noreply.github.com>
2025-08-03 19:44:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,17 +27,17 @@ const DEFAULT_TERMINALS = {
         name: 'aios',
         command: 'ssh root@IP -t "if [ -f /usr/bin/aios ]; then /usr/bin/aios; else wget -q -O /usr/bin/aios https://raw.githubusercontent.com/site-u2023/aios/main/aios && chmod +x /usr/bin/aios && /usr/bin/aios; fi"'
     },
-    aios_clear_hosts: { 
-        name: 'aios (clear hosts)', 
-        command: 'ssh -o StrictHostKeyChecking=no -o HostKeyAlgorithms=+ssh-rsa root@IP -t "if [ -f /usr/bin/aios ]; then /usr/bin/aios; else wget -q -O /usr/bin/aios https://raw.githubusercontent.com/site-u2023/aios/main/aios && chmod +x /usr/bin/aios && /usr/bin/aios; fi"' 
+    aios_clear_hosts: {
+        name: 'aios (clear hosts)',
+        command: 'ssh -o StrictHostKeyChecking=no root@IP -t "if [ -f /usr/bin/aios ]; then /usr/bin/aios; else wget -q -O /usr/bin/aios https://raw.githubusercontent.com/site-u2023/aios/main/aios && chmod +x /usr/bin/aios && /usr/bin/aios; fi"'
     },
     ssh: {
         name: 'SSH',
         command: 'ssh'
     },
-    ssh_clear_hosts: { 
-        name: 'SSH (clear hosts)', 
-        command: 'ssh -o StrictHostKeyChecking=no -o HostKeyAlgorithms=+ssh-rsa' 
+    ssh_clear_hosts: {
+        name: 'SSH (clear hosts)',
+        command: 'ssh -o StrictHostKeyChecking=no'
     }
 };
 
