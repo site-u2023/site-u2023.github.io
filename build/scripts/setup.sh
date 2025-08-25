97fe9010fc8e0a80b2480bc374cce7d6c34a8511
site_u <140032047+site-u2023@users.noreply.github.com>
2025-08-25 19:33:06 +0900

    Update setup.sh

--- build/scripts/setup.sh
+++ build/scripts/setup.sh
@@ -123,7 +123,7 @@ exec >/tmp/setup.log 2>&1
     uci add_list firewall.@zone[1].network="\${MAP6_NAME}"
     uci set firewall.@zone[1].masq='1'
     uci set firewall.@zone[1].mtu_fix='1'
-    cat > /lib/netifd/proto/map.sh << 'MAP_SH_EOF'
+    cat > /lib/netifd/proto/map.sh <<'MAP_SH_EOF'
 \${map_sh_content}
 MAP_SH_EOF
 }
@@ -186,30 +186,25 @@ MAP_SH_EOF
 # BEGIN_CUSTOM_COMMANDS
 # END_CUSTOM_COMMANDS
 uci commit 2>/dev/null
-# enable_netopt="1"
-[ -n "\${enable_netopt}" ] && { cat > /etc/rc.local << 'EOF'
+[ -n "\${enable_netopt}" ] && { cat > /etc/rc.local <<'EOF'
 #!/bin/sh
 C=/etc/sysctl.d/99-net-opt.conf
 M=\$(awk '/MemTotal/{print int(\$2/1024)}' /proc/meminfo)
 P=\$(grep -c ^processor /proc/cpuinfo)
-if [ \$M -ge 3072 ]; then R=16777216 W=16777216 TR="4096 262144 16777216" TW=\$TR CT=262144 NB=5000 SC=16384
-elif [ \$M -ge 1536 ]; then R=8388608 W=8388608 TR="4096 131072 8388608" TW=\$TR CT=131072 NB=2500 SC=8192
-elif [ \$M -ge 512 ]; then R=4194304 W=4194304 TR="4096 65536 4194304" TW=\$TR CT=65536 NB=1000 SC=4096
+if [ \$M -ge 3072 ]; then R=16777216 W=16777216 TR="${netopt_rmem:-4096 262144 16777216}" TW="${netopt_wmem:-4096 262144 16777216}" CT=${netopt_conntrack:-262144} NB=${netopt_backlog:-5000} SC=${netopt_somaxconn:-16384}
+elif [ \$M -ge 1536 ]; then R=8388608 W=8388608 TR="${netopt_rmem:-4096 131072 8388608}" TW="${netopt_wmem:-4096 131072 8388608}" CT=${netopt_conntrack:-131072} NB=${netopt_backlog:-2500} SC=${netopt_somaxconn:-8192}
+elif [ \$M -ge 512 ]; then R=4194304 W=4194304 TR="${netopt_rmem:-4096 65536 4194304}" TW="${netopt_wmem:-4096 65536 4194304}" CT=${netopt_conntrack:-65536} NB=${netopt_backlog:-1000} SC=${netopt_somaxconn:-4096}
 else exit 0; fi
 [ \$P -gt 4 ] && { NB=\$((NB*2)); SC=\$((SC*2)); }
 [ \$P -gt 2 ] && [ \$P -le 4 ] && { NB=\$((NB*3/2)); SC=\$((SC*3/2)); }
-printf "net.core.rmem_max=%s\\nnet.core.wmem_max=%s\\nnet.ipv4.tcp_rmem=%s\\nnet.ipv4.tcp_wmem=%s\\nnet.ipv4.tcp_congestion_control=cubic\\nnet.ipv4.tcp_fastopen=3\\nnet.netfilter.nf_conntrack_max=%s\\nnet.core.netdev_max_backlog=%s\\nnet.core.somaxconn=%s\\n" "\$R" "\$W" "\$TR" "\$TW" "\$CT" "\$NB" "\$SC" > \$C
+printf "net.core.rmem_max=%s\\nnet.core.wmem_max=%s\\nnet.ipv4.tcp_rmem=%s\\nnet.ipv4.tcp_wmem=%s\\nnet.ipv4.tcp_congestion_control=${netopt_congestion:-cubic}\\nnet.ipv4.tcp_fastopen=3\\nnet.netfilter.nf_conntrack_max=%s\\nnet.core.netdev_max_backlog=%s\\nnet.core.somaxconn=%s\\n" "\$R" "\$W" "\$TR" "\$TW" "\$CT" "\$NB" "\$SC" > \$C
 sysctl -p \$C
-cat >/etc/rc.local <<EOF
-# Put your custom commands here that should be executed once
-# the system init process has finished. By default this file does nothing.
-
+cat > /etc/rc.local <<'RESET_EOF'
 exit 0
-EOF
+RESET_EOF
 exit 0
 EOF
 }
-sed -i '$i (for i in $(uci show network|grep "=interface"|cut -d. -f2|cut -d= -f1); do ifup $i; done; sleep 5; [ -x /etc/init.d/odhcpd ] && /etc/init.d/odhcpd restart; sed -i '\''/ifup /d;/odhcpd restart/d'\'' /etc/rc.local) &' /etc/rc.local
 [ -n "\${backup_path}" ] && sysupgrade -q -k -b "\${backup_path}"
 echo "All done!"
 exit 0
