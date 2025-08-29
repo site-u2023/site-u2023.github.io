ecb733c5cdb23f73d73937e2f31195afabe79622
site_u <140032047+site-u2023@users.noreply.github.com>
2025-08-29 16:02:51 +0900

    Create config.js

---
+++ builder/config.js
@@ -0,0 +1,8 @@
+const config = {
+    show_help: true,
+    image_url: "https://downloads.openwrt.org",
+    info_url: "https://openwrt.org/start?q={title}&do=search",
+    asu_url: "https://sysupgrade.openwrt.org",
+    asu_extra_packages: ["luci"],
+    min_version: "21.02.0",  // 最小サポートバージョン
+};
