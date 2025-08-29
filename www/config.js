400dd5bb176859a91dfce3c10e617c06ad9a4319
site_u <140032047+site-u2023@users.noreply.github.com>
2025-08-29 21:33:24 +0900

    Create config.js

---
+++ www/config.js
@@ -0,0 +1,25 @@
+/* exported config */
+
+var config = {
+  // Show help text for images
+  show_help: true,
+
+  // Versions list (optional if provided by .versions.json)
+  versions: ["23.05.4", "19.07.10"],
+
+  // Pre-selected version (optional if provided by .versions.json)
+  default_version: "23.05.4",
+
+  // Image download URL (e.g. "https://downloads.openwrt.org")
+  image_url: "../misc",
+
+  // Insert snapshot versions (optional)
+  //show_snapshots: true,
+
+  // Info link URL (optional)
+  info_url: "https://openwrt.org/start?do=search&id=toh&q={title} @toh",
+
+  // Attended Sysupgrade Server support (optional)
+  asu_url: "https://sysupgrade.openwrt.org",
+  asu_extra_packages: ["luci"],
+};
