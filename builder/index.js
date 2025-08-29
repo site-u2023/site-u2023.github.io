4e94d2fc7af992538081bbb2c92383a42bafd6a4
site_u <140032047+site-u2023@users.noreply.github.com>
2025-08-29 16:02:19 +0900

    Update index.js

--- builder/index.js
+++ builder/index.js
@@ -1,13 +1,3 @@
-// config.js相当の設定
-const config = {
-    show_help: true,
-    image_url: "https://downloads.openwrt.org",
-    info_url: "https://openwrt.org/start?q={title}&do=search",
-    asu_url: "https://sysupgrade.openwrt.org",
-    asu_extra_packages: ["luci"],
-    min_version: "21.02.0",  // 最小サポートバージョン
-};
-
 async function loadSetupScript() {
   const res = await fetch('scripts/setup.sh');
   const setupScript = await res.text();
