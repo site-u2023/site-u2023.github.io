41c389bd3da096b803535b9a1b19527f52a62c07
site_u <140032047+site-u2023@users.noreply.github.com>
2025-08-30 11:37:08 +0900

    Update config.js

--- www/config.js
+++ www/config.js
@@ -5,21 +5,24 @@ var config = {
   show_help: true,
 
   // Versions list (optional if provided by .versions.json)
-  versions: ["23.05.4", "19.07.10"],
+  versions: [],
 
   // Pre-selected version (optional if provided by .versions.json)
-  default_version: "23.05.4",
+  default_version: "",
 
   // Image download URL (e.g. "https://downloads.openwrt.org")
-  image_url: "../misc",
+  image_url: "https://downloads.openwrt.org",
 
   // Insert snapshot versions (optional)
   //show_snapshots: true,
 
   // Info link URL (optional)
-  info_url: "https://openwrt.org/start?do=search&id=toh&q={title} @toh",
+  info_url: "https://openwrt.org/start?q={title}&do=search",
 
   // Attended Sysupgrade Server support (optional)
   asu_url: "https://sysupgrade.openwrt.org",
   asu_extra_packages: ["luci"],
+
+  // 最小サポートバージョン（任意）
+  min_version: "21.02.0"
 };
