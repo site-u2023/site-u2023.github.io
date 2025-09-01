fe213616045caa5f8e2d06e96902ecae44bdbfa8
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-01 20:04:34 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -451,7 +451,6 @@ function loadUciDefaultsTemplate() {
         })
         .then(text => {
             textarea.value = text;
-            autoResizeTextarea(textarea);
             console.log('setup.sh loaded successfully');
         })
         .catch(err => {
