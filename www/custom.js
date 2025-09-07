7c9576b1bbc44cd0687d5f7f573390d80aa2070e
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-07 22:21:52 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -118,6 +118,7 @@ function replaceAsuSection(asuSection, temp) {
     
     const customPackages = temp.querySelector('#custom-packages-section details');
     const customScripts = temp.querySelector('#custom-scripts-section details');
+
     if (customPackages) {
         customPackages.id = 'custom-packages-details';
         newDiv.appendChild(customPackages);
@@ -126,13 +127,7 @@ function replaceAsuSection(asuSection, temp) {
         customScripts.id = 'custom-scripts-details';
         newDiv.appendChild(customScripts);
     }
-    
-    // 既存の#asu-packagesテキストエリアを保持
-    const existingPackagesTextarea = asuSection.querySelector('#asu-packages');
-    if (existingPackagesTextarea) {
-        newDiv.appendChild(existingPackagesTextarea.cloneNode(true));
-    }
-    
+
     // index.jsが期待する全てのDOM要素を追加
     newDiv.insertAdjacentHTML('beforeend', `
         <br>
