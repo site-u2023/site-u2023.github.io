6485409393f0ccf8d0f5f4a3766bbc41b02073a6
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-08 10:15:51 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -1725,7 +1725,7 @@ function loadUciDefaultsTemplate() {
 
     function autoResize() {
         const lines = textarea.value.split('\n').length;
-        textarea.style.height = lines + 1;
+        textarea.style.height = lines;
     }
 
     textarea.addEventListener('input', autoResize);
