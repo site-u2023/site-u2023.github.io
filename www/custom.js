b6e43ccaeb4a553421c7aed0315e077a165bbc04
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-10 01:23:22 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -2061,32 +2061,33 @@ function updateAutoConnectionInfo(apiInfo) {
     if (!autoInfo) return;
 
     let infoText = '';
+    
+    if (apiInfo?.mape?.brIpv6Address) {
+        infoText = '🌐\u00A0Detected: MAP-E\n';
+        infoText += `\u00A0\u00A0\u00A0\u00A0\u00A0BR: ${apiInfo.mape.brIpv6Address}\n`;
+        infoText += `\u00A0\u00A0\u00A0\u00A0\u00A0EA-len: ${apiInfo.mape.eaBitLength}\n`;
+        infoText += `\u00A0\u00A0\u00A0\u00A0\u00A0IPv4 Prefix: ${apiInfo.mape.ipv4Prefix}/${apiInfo.mape.ipv4PrefixLength}\n`;
+        infoText += `\u00A0\u00A0\u00A0\u00A0\u00A0IPv6 Prefix: ${apiInfo.mape.ipv6Prefix}/${apiInfo.mape.ipv6PrefixLength}\n`;
+        infoText += `\u00A0\u00A0\u00A0\u00A0\u00A0PSID: offset=${apiInfo.mape.psIdOffset}\n`;
+        infoText += `\u00A0\u00A0\u00A0\u00A0\u00A0PSID: length=${apiInfo.mape.psidlen}`;
+    } else if (apiInfo?.aftr) {
+        infoText = '🌐\u00A0Detected: DS-Lite\n';
+        infoText += `\u00A0\u00A0\u00A0\u00A0\u00A0AFTR: ${apiInfo.aftr}`;
+    } else if (apiInfo) {
+        infoText = '🌐\u00A0Detected: DHCP/PPPoE\n';
+        infoText += '\u00A0\u00A0\u00A0\u00A0\u00A0Standard connection will be used';
+    } else {
+        infoText = '⚠\u00A0No connection information available\n';
+        infoText += '\u00A0\u00A0\u00A0\u00A0\u00A0Please select connection type manually';
+    }
 
     if (apiInfo?.isp) {
-        infoText += `\n\n📡 ISP: ${apiInfo.isp}`;
+        infoText += `\n\n🌐\u00A0ISP: ${apiInfo.isp}`;
         if (apiInfo.as) {
-            infoText += ` (${apiInfo.as})`;
+            infoText += `\n\u00A0\u00A0\u00A0\u00A0\u00A0AS: ${apiInfo.as}`;
         }
     }
     
-    if (apiInfo?.mape?.brIpv6Address) {
-        infoText = '🌐 Detected: MAP-E\n';
-        infoText += `\u00A0\u00A0\u00A0\u00A0BR: ${apiInfo.mape.brIpv6Address}\n`;
-        infoText += `\u00A0\u00A0\u00A0\u00A0EA-len: ${apiInfo.mape.eaBitLength}\n`;
-        infoText += `\u00A0\u00A0\u00A0\u00A0IPv4 Prefix: ${apiInfo.mape.ipv4Prefix}/${apiInfo.mape.ipv4PrefixLength}\n`;
-        infoText += `\u00A0\u00A0\u00A0\u00A0IPv6 Prefix: ${apiInfo.mape.ipv6Prefix}/${apiInfo.mape.ipv6PrefixLength}\n`;
-        infoText += `\u00A0\u00A0\u00A0\u00A0PSID: offset=${apiInfo.mape.psIdOffset}, length=${apiInfo.mape.psidlen}`;
-    } else if (apiInfo?.aftr) {
-        infoText = '🌐 Detected: DS-Lite\n';
-        infoText += `\u00A0\u00A0\u00A0\u00A0AFTR: ${apiInfo.aftr}`;
-    } else if (apiInfo) {
-        infoText = '🌐 Detected: DHCP/PPPoE\n';
-        infoText += '\u00A0\u00A0\u00A0\u00A0Standard connection will be used';
-    } else {
-        infoText = '⚠ No connection information available\n';
-        infoText += '\u00A0\u00A0\u00A0\u00A0Please select connection type manually';
-    }
-
     autoInfo.textContent = infoText;
 }
 
