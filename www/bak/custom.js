64ae21a075c7042e7214692a3a431ceebfc7c306
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-11 18:50:02 +0900

    Update custom.js

--- www/bak/custom.js
+++ www/bak/custom.js
@@ -930,11 +930,6 @@ function applyCustomTranslations(map) {
     }
 }
 
-function triggerPackageUpdate() {
-    updateLanguagePackage();
-    updateVariableDefinitions();
-}
-
 function extractLuciName(pkg) {
     if (pkg === 'luci') return 'base';
 
@@ -1400,9 +1395,9 @@ function handleRadioChange(e) {
 
 function updatePackageListFromDynamicSources() {
     updateSetupJsonPackages();
-    triggerPackageUpdate();
+    updateLanguagePackage();
+    updateVariableDefinitions();
 }
-
 
 function updateSetupJsonPackages() {
     if (!setupConfig) return;
@@ -2510,10 +2505,10 @@ function handlePackageSelection(e) {
     
     const dependencies = pkg.getAttribute('data-dependencies');
     if (dependencies) {
-        dependencies.split(',').forEach(depName => {  // depNameは表示名
-            const depPkg = findPackageById(depName);  // 表示名でパッケージを検索
+        dependencies.split(',').forEach(depName => {
+            const depPkg = findPackageById(depName);
             if (depPkg) {
-                const depCheckbox = document.querySelector(`[data-unique-id="${depPkg.uniqueId || depPkg.id}"]`);  // パッケージのnameでチェックボックスを探す
+                const depCheckbox = document.querySelector(`[data-unique-id="${depPkg.uniqueId || depPkg.id}"]`);
                 if (depCheckbox) {
                     depCheckbox.checked = isChecked;
                     
@@ -2531,14 +2526,9 @@ function handlePackageSelection(e) {
             }
         });
     }
-    
-    const enableVar = pkg.getAttribute('data-enable-var');
-    if (enableVar) {
-        updateVariableDefinitions();
-    }
-    
-    triggerPackageUpdate();
+    updatePackageListFromDynamicSources();
 }
+
 
 // パッケージリスト更新（Postinstテキストエリアへの反映）
 function updatePackageListFromSelector() {
@@ -2678,21 +2668,6 @@ function updateVariableDefinitions() {
     
     const variableDefinitions = generateVariableDefinitions(emissionValues);
     updateTextareaContent(textarea, variableDefinitions);
-
-    // 以下は既存のテキストエリア更新処理
-    let content = textarea.value;
-    const beginMarker = '# BEGIN_VARIABLE_DEFINITIONS';
-    const endMarker = '# END_VARIABLE_DEFINITIONS';
-    const beginIndex = content.indexOf(beginMarker);
-    const endIndex = content.indexOf(endMarker);
-
-    if (beginIndex !== -1 && endIndex !== -1) {
-        const beforeSection = content.substring(0, beginIndex + beginMarker.length);
-        const afterSection = content.substring(endIndex);
-        const newSection = variableDefinitions ? '\n' + variableDefinitions + '\n' : '\n';
-        textarea.value = beforeSection + newSection + afterSection;
-        textarea.rows = textarea.value.split('\n').length + 1;
-    }
 }
 
 // テキストエリア更新の共通処理
