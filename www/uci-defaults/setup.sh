367475fcb3e7b7ec659f9b08330205cce47de916
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-22 01:44:19 +0900

    Update setup.sh

--- www/uci-defaults/setup.sh
+++ www/uci-defaults/setup.sh
@@ -467,7 +467,7 @@ SAMBA_EOF
     [ -f /boot/config.txt ] && ! grep -q 'dtoverlay=dwc2' /boot/config.txt && echo 'dtoverlay=dwc2' >> /boot/config.txt
     [ -f /boot/cmdline.txt ] && sed -i 's/rootwait/& modules-load=dwc2,g_ether/' /boot/cmdline.txt
     printf '%s\n%s\n' "dwc2" "g_ether" > /etc/modules.d/99-gadget
-	uci add_list network.lan.device='usb0'
+	uci add_list network.${LAN}.ports='usb0'
 }
 [ -n "${enable_netopt}" ] && [ "$MEM" -ge 400 ] && {
     C=/etc/sysctl.d/99-net-opt.conf
