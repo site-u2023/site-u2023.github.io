8640529426501861fb8ea2fb00896c2a1bfcd3a3
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-22 09:06:05 +0900

    Update setup.sh

--- www/uci-defaults/setup.sh
+++ www/uci-defaults/setup.sh
@@ -466,10 +466,10 @@ SAMBA_EOF
 [ -n "${enable_usb_gadget}" ] && {
     [ -f /boot/config.txt ] && ! grep -q 'dtoverlay=dwc2' /boot/config.txt && echo 'dtoverlay=dwc2' >> /boot/config.txt
     [ -f /boot/cmdline.txt ] && sed -i 's/rootwait/& modules-load=dwc2,g_ether/' /boot/cmdline.txt
-    printf '%s\n%s\n' "dwc2" "g_ether" > /etc/modules.d/99-gadget
-	modprobe dwc2 2>/dev/null
-    modprobe g_ether 2>/dev/null
-    uci add_list network.@device[0].ports="usb0"
+	printf '%s\n%s\n' "dwc2" "g_ether" > /etc/modules.d/99-gadget
+ 	modprobe dwc2 2>/dev/null
+ 	modprobe g_ether 2>/dev/null
+  	uci add_list network.@device[0].ports="usb0"
 }
 [ -n "${enable_netopt}" ] && [ "$MEM" -ge 400 ] && {
     C=/etc/sysctl.d/99-net-opt.conf
