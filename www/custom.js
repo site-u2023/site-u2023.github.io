d7e78bd2244ac5a6c67fe60acf125515c75fdca4
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-25 11:49:24 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -3211,9 +3211,7 @@ function updateAutoConnectionInfo(apiInfo) {
         infoText += `export LEGACY=1<br>`;
         infoText += `<hr>`;
         infoText += `<span style="color:DarkBlue">(config-softwire)#</span> <strong>map-version draft</strong><br>`;
-        infoText += `<span style="color:DarkBlue">(config-softwire)#</span> rule <span style="color:blue">&lt;0-65535&gt;</span> <strong>ipv4-prefix</strong> <span style="color:blue">${apiInfo.mape.ipv4Prefix}/${apiInfo.mape.ipv4PrefixLength}</span> <strong>ipv6-prefix</strong> <span style="color:blue">${apiInfo.mape.ipv6Prefix}/${apiInfo.mape.ipv6PrefixLength}</span> [<strong>ea-length</strong> <span style="color:blue">${apiInfo.mape.eaBitLength}</span>|<strong>psid-length</strong> <span style="color:lime">${apiInfo.mape.psidlen}</span>] [<strong>offset</strong> <span style="color:blue">${apiInfo.mape.psIdOffset}</span>] [<strong>forwarding</strong>]<br>`;
-        infoText += `<hr>`;
-        infoText += `<div style="text-align: center;"><a href="https://ipv4.web.fc2.com/map-e.html" target="_blank">Powered by https://ipv4.web.fc2.com/map-e.html</a></div>`;
+        infoText += `<span style="color:DarkBlue">(config-softwire)#</span> <strong>rule <span style="color:blue">&lt;0-65535&gt;</span> ipv4-prefix <span style="color:blue">${apiInfo.mape.ipv4Prefix}/${apiInfo.mape.ipv4PrefixLength}</span> ipv6-prefix <span style="color:blue">${apiInfo.mape.ipv6Prefix}/${apiInfo.mape.ipv6PrefixLength}</span></strong> [ea-length <span style="color:blue">${apiInfo.mape.eaBitLength}</span>|psid-length <span style="color:blue">${apiInfo.mape.psidlen}</span>] [offset <span style="color:blue">${apiInfo.mape.psIdOffset}</span>] [forwarding]<br>`;
     } else if (connectionType === 'DS-Lite') {
         infoText += `${current_language_json['tr-auto-detection'] || 'Auto Detection:'} ${connectionType}<br>`;
         infoText += `<hr>`;
