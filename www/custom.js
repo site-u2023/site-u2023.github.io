155fc9599b2c34966c70ddab78782bcff2aab6e4
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-26 11:04:03 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -3153,7 +3153,7 @@ async function initializeCustomFeatures(asuSection, temp) {
             .concat(state.packages.device)
             .concat(state.packages.extra);
 
-        const textarea = document.querySelector('#asu-packages');
+        const textarea = document.querySelector("#uci-defaults-details #uci-defaults-content");
         if (textarea && initialPackages.length > 0) {
             UI.updateElement(textarea, { value: initialPackages.join(' ') });
             console.log('Device packages force applied:', initialPackages);
