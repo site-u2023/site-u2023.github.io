c8d294d6f5c5567ed58ec7742db4716b0c59ea54
site_u <140032047+site-u2023@users.noreply.github.com>
2025-09-26 11:39:17 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -3698,13 +3698,7 @@ function updateTextareaContent(textarea, variableDefinitions) {
         textarea.value = beforeSection + newSection + afterSection;
         textarea.rows = textarea.value.split('\n').length + 1;
         
-        const lines = textarea.value.replace(/\n$/, '').split('\n').length;
-        const bytes = new Blob([textarea.value]).size;
-        const kb = (bytes / 1024).toFixed(1);
-        const sizeElement = document.querySelector('#uci-defaults-size');
-        if (sizeElement) {
-            sizeElement.textContent = `${lines} lines · ${kb} KB`;
-        }
+        updateFileSize(textarea.value);
     }
 }
 
