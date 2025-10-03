06bc3d73b5aceabf88bc08875fcb28325db1845c
site_u <140032047+site-u2023@users.noreply.github.com>
2025-10-04 02:38:10 +0900

    Update config.js

--- www/config.js
+++ www/config.js
@@ -31,7 +31,7 @@ var config = {
   asu_extra_packages: ["luci"],
 
   // 最小サポートバージョン（任意）
-  min_version: "21.02.0",
+  min_version: "22.03.0",
   
   // Auto-configuration API URL
   auto_config_api_url: "https://auto-config.site-u.workers.dev/",
