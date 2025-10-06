d5291807171c175f1128af1204a0fc147406fa7e
site_u <140032047+site-u2023@users.noreply.github.com>
2025-10-06 21:25:56 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -1715,7 +1715,7 @@ function getFieldsForWifiMode(mode) {
     
     fields.push('wlan_ssid', 'wlan_password');
     
-    if (mode === 'luci-app-usteer') {
+    if (mode === 'usteer') {
         fields.push('mobility_domain', 'snr');
     }
     
