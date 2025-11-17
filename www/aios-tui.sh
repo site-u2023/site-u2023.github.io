9784baef3a203a85f5a95efab621cd7660fdf482
site_u <140032047+site-u2023@users.noreply.github.com>
2025-11-17 22:48:02 +0900

    Update aios-tui.sh

--- www/aios-tui.sh
+++ www/aios-tui.sh
@@ -1350,7 +1350,7 @@ whiptail_category_config() {
             if [ "$conn_type" = "auto" ]; then
                 continue
             elif [ "$conn_type" = "dhcp" ]; then
-                show_msgbox "$breadcrumb" "$(translate 'tr-dhcp'):\n\nDHCP"
+                show_menu "$breadcrumb" "$(translate 'tr-dhcp'):" "" "" "1" "DHCP"
             fi
         fi
         
