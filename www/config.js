ae7f16989f335e77bceb7fe59fef67a23b5452b0
site_u <140032047+site-u2023@users.noreply.github.com>
2025-11-18 10:29:25 +0900

    Update config.js

--- www/config.js
+++ www/config.js
@@ -47,6 +47,11 @@ var config = {
   // Language file path template
   language_path_template: "langs/custom.{lang}.json",
   
+  // Custom feeds database URL
+  customfeeds_db_path: "custom-feeds/customfeeds.json",
+  // Custom feeds template URL (gSpotx2f)
+  customfeeds_template_path: "custom-feeds/gSpotx2f.sh",
+  
   // LuCI OPKG 検索エンドポイント
   // https://downloads.openwrt.org/releases/24.10.2/packages/aarch64_cortex-a53/packages/Packages
   opkg_search_url: "https://downloads.openwrt.org/releases/{version}/packages/{arch}/{feed}/Packages",
