dab13bc856cb6ff2eb3c5e1a26c8b46d649d9bc8
site_u <140032047+site-u2023@users.noreply.github.com>
2025-11-18 17:40:04 +0900

    Update gSpotx2f.sh

--- www/custom-feeds/gSpotx2f.sh
+++ www/custom-feeds/gSpotx2f.sh
@@ -40,7 +40,17 @@ fi
 
 # Process each package in PACKAGES variable
 # Format: "pattern:exclude:filename:enable_service:restart_service"
-echo "$PACKAGES" | tr ' ' '\n' | while IFS=':' read -r pattern exclude filename enable_service restart_service; do
+echo "$PACKAGES" | tr ' ' '\n' | while read -r line; do
+    [ -z "$line" ] && continue
+    set -- $line
+    pattern="$1"
+    exclude="$2"
+    filename="$3"
+    enable_service="$4"
+    restart_service="$5"
+    [ "$exclude" = "_none_" ] && exclude=""
+    [ "$enable_service" = "_none_" ] && enable_service=""
+    [ "$restart_service" = "_none_" ] && restart_service=""
     [ -z "$pattern" ] && continue
     
     echo ""
