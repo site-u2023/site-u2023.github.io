667747c4e8f74ff0d45e365eeda64eeadeda660a
site_u <140032047+site-u2023@users.noreply.github.com>
2025-11-20 10:29:39 +0900

    Update aios-tui2.sh

--- www/aios-tui2.sh
+++ www/aios-tui2.sh
@@ -3,7 +3,7 @@
 # ASU (Attended SysUpgrade) Compatible
 # Common Functions (UI-independent)
 
-VERSION="R7.1120.0022"
+VERSION="R7.1120.0029"
 BASE_TMP_DIR="/tmp"
 CONFIG_DIR="$BASE_TMP_DIR/aiost"
 BOOTSTRAP_URL="https://site-u.pages.dev/www"
