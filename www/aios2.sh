a1165a904839fb21ae202f579a13dd077e5d69f6
site_u <140032047+site-u2023@users.noreply.github.com>
2025-11-21 22:22:11 +0900

    Update aios2.sh

--- www/aios2.sh
+++ www/aios2.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-# shellcheck shell=sh
-# shellcheck disable=SC2034
-# shellcheck disable=SC3043
+# shellcheck shell=sh disable=SC2034,SC3043
 # OpenWrt Device Setup Tool - CLI Version
 # ASU (Attended SysUpgrade) Compatible
 # Common Functions (UI-independent)
