b8688d678e534039c195f4984294fcd6898fd1bd
site_u <140032047+site-u2023@users.noreply.github.com>
2025-11-26 11:41:32 +0900

    Update aios2.sh

--- www/aios2.sh
+++ www/aios2.sh
@@ -39,7 +39,7 @@ XXX_print_banner_unicode() {
     printf "\n"
 }
 
-print_banner_unicode() {
+XXX_print_banner_unicode() {
     printf "\n"
     printf       "\033[35m               ██ █\033[0m\n"
     printf       "\033[34m     ████      ███       ████      █████\033[0m       \033[37m█████\033[0m\n"
@@ -49,6 +49,19 @@ print_banner_unicode() {
     printf       "\033[31m     █████     ████      ████     ██████\033[0m      \033[37m██████\033[0m\n"
     printf "\n"
     printf       "\033[37m                   Vr.%s\033[0m\n" "$VERSION"
+    printf "\n"
+}
+
+print_banner_unicode() {
+    printf "\n"
+    printf       "\033[35m             ██ █\033[0m\n"
+    printf       "\033[34m    ████     ███      ████     █████\033[0m      \033[37m█████\033[0m\n"
+    printf       "\033[32m       ██     ██     ██  ██   ██\033[0m              \033[37m██\033[0m\n"
+    printf       "\033[33m    █████     ██     ██  ██    █████\033[0m      \033[37m█████\033[0m\n"
+    printf "\033[38;5;208m   ██  ██     ██     ██  ██        ██\033[0m   \033[37m ██\033[0m\n"
+    printf       "\033[31m    █████    ████     ████    ██████\033[0m     \033[37m██████\033[0m\n"
+    printf "\n"
+    printf       "\033[37m                Vr.%s\033[0m\n" "$VERSION"
     printf "\n"
 }
 
