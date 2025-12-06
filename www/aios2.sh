a47852fea1d60ff8e9f76c2ed269b0e04b8c1770
site_u <140032047+site-u2023@users.noreply.github.com>
2025-12-06 21:40:07 +0900

    Update aios2.sh

--- www/aios2.sh
+++ www/aios2.sh
@@ -4,7 +4,7 @@
 # ASU (Attended SysUpgrade) Compatible
 # Common Functions (UI-independent)
 
-VERSION="R7.1206.2121"
+VERSION="R7.1206.2139"
 
 # =============================================================================
 # Package Selection and Installation Logic
