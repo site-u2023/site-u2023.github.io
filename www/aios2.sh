ef6f857c4a4dd91c7f107ffeb245c8dcfc78d1e7
site_u <140032047+site-u2023@users.noreply.github.com>
2025-12-06 17:11:56 +0900

    Update aios2.sh

--- www/aios2.sh
+++ www/aios2.sh
@@ -1080,7 +1080,7 @@ get_customfeed_categories() {
     jsonfilter -i "$CUSTOMFEEDS_JSON" -e '@.categories[*].id' 2>/dev/null | grep -v '^$'
 }
 
-custom_feeds_selection_common() {
+XXX_custom_feeds_selection_common() {
     download_customfeeds_json || return 1
     
     local tr_main_menu tr_custom_feeds breadcrumb categories
@@ -1102,6 +1102,20 @@ custom_feeds_selection_common() {
     echo "$categories"
     echo "EOF"
     
+    return 0
+}
+
+custom_feeds_selection_common() {
+    download_customfeeds_json || return 1
+    
+    local categories
+    categories=$(get_customfeed_categories)
+    
+    if [ -z "$categories" ]; then
+        return 1
+    fi
+    
+    echo "$categories"
     return 0
 }
 
