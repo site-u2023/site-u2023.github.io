c552ae8bb3bbeba428604e6136d2bb3c8d254c17
site_u <140032047+site-u2023@users.noreply.github.com>
2025-12-12 17:23:05 +0900

    Update aios2-whiptail.sh

--- www/aios2-whiptail.sh
+++ www/aios2-whiptail.sh
@@ -3,8 +3,8 @@
 # OpenWrt Device Setup Tool - whiptail TUI Module
 # This file contains whiptail-specific UI functions
 
-VERSION="R7.1210.1144"
-TITLE="aios2"
+VERSION="R7.1212.1722"
+TITLE="all in one scripts 2"
 
 UI_WIDTH="78"
 UI_HEIGHT="0"
