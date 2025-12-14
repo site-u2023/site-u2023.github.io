f73e8e2c85fea67bd24fb0cb764a44596c27ba60
site_u <140032047+site-u2023@users.noreply.github.com>
2025-12-14 11:26:16 +0900

    Update aios2.sh

--- www/aios2.sh
+++ www/aios2.sh
@@ -4,7 +4,8 @@
 # ASU (Attended SysUpgrade) Compatible
 # Common Functions (UI-independent)
 
-# パッケージ表示要件
+# パッケージ要件
+# 本スクリプトでは初動でデバイス名確定後、実行中の変更は無い
 # 【uniqueId の扱い】
 #   - uniqueId がある場合、判定には必ず uniqueId を使用（id は使わない）
 #   - uniqueId がない場合のみ id で判定
