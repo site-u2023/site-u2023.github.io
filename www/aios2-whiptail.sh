56585741520a8dc89eaec9616e8f4c22091f4f53
site_u <140032047+site-u2023@users.noreply.github.com>
2025-12-17 13:40:41 +0900

    Update aios2-whiptail.sh

--- www/aios2-whiptail.sh
+++ www/aios2-whiptail.sh
@@ -916,12 +916,10 @@ show_language_selector() {
     local title selected
     title=$(translate "tr-tui-language-package")
     
-    eval "$DIALOG --title \"$title\" \
-        --radiolist \"$(translate 'tr-tui-select-language')\" \
+    $DIALOG --title "$title" \
+        --radiolist "$(translate 'tr-tui-select-language')" \
         $DIALOG_HEIGHT $DIALOG_WIDTH $LIST_HEIGHT \
-        $radio_list 3>&1 1>&2 2>&3" > /tmp/lang_choice.txt
-    selected=$(cat /tmp/lang_choice.txt)
-    rm -f /tmp/lang_choice.txt
+        $(echo "$radio_list") 3>&1 1>&2 2>&3
     
     [ $? -ne 0 ] || [ -z "$selected" ] && return 0
     [ "$selected" = "$current_lang" ] && return 0
