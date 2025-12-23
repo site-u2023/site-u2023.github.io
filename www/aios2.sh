cf1b6e0282595f5dd2c92b2a6dd854bb5b672cd2
site_u <140032047+site-u2023@users.noreply.github.com>
2025-12-23 23:20:13 +0900

    Update aios2.sh

--- www/aios2.sh
+++ www/aios2.sh
@@ -2970,7 +2970,7 @@ EOF
     return 1
 }
 
-auto_add_conditional_packages() {
+OK_XXXXX_auto_add_conditional_packages() {
     local cat_id="$1"
     local effective_conn_type
     
@@ -3122,7 +3122,7 @@ EOF
     debug_log "=== auto_add_conditional_packages finished ==="
 }
 
-NG_XXXXX_auto_add_conditional_packages() {
+auto_add_conditional_packages() {
     local cat_id="$1"
     local effective_conn_type
     
