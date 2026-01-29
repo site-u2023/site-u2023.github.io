9e3adc3ce4338bae561b94bcd47c36a756ffb6a6
site_u <140032047+site-u2023@users.noreply.github.com>
2026-01-29 11:05:04 +0900

    Update setup_mape_ext.sh

--- www/uci-defaults/setup_mape_ext.sh
+++ www/uci-defaults/setup_mape_ext.sh
@@ -6,9 +6,16 @@ echo "=== MAP-Eポートセット拡張設定開始 ==="
 
 # 1. 必要モジュールの確認
 opkg update
-opkg install iptables-mod-ipopt
+opkg install kmod-ipt-ipopt
 
-# 2. MAPパラメータ取得
+# 2. firewall.user バックアップ
+[ -f /etc/firewall.user ] && cp /etc/firewall.user /etc/firewall.user.bak_$(date +%Y%m%d)
+
+# 3. 新しいルール書き込み
+cat > /etc/firewall.user << 'EOF'
+# MAP-E Port Set Expansion (全ポートセット活用)
+
+# MAPパラメータ取得
 API_RESPONSE="$(wget -qO- https://auto-config.site-u.workers.dev/)"
 PSID=$(echo "$API_RESPONSE" | jsonfilter -e '@.mape.psid')
 PSIDLEN=$(echo "$API_RESPONSE" | jsonfilter -e '@.mape.psidlen')
@@ -20,59 +27,39 @@ UNITS=$((1 << PSIDLEN))
 PORT_SET_WIDTH=$((65536 / UNITS))
 PORTS_PER_RULE=$((PORT_SET_WIDTH / 16))
 
-echo "PSID=$PSID, PSIDLEN=$PSIDLEN, UNITS=$UNITS"
-echo "PORT_SET_WIDTH=$PORT_SET_WIDTH, PORTS_PER_RULE=$PORTS_PER_RULE"
-
-# 3. MAP-Eインターフェース自動検出
+# MAP-Eインターフェース自動検出
 TUNDEV=$(ip -o link show | grep 'map-' | awk '{print $2}' | cut -d@ -f1 | head -n 1)
 TUNDEV=${TUNDEV:-map-mape}
 
-# 4. WANのIPv4アドレス取得
+# WANのIPv4アドレス取得
 . /lib/functions/network.sh
 network_flush_cache
 network_find_wan NET_IF
 network_get_ipaddr NET_ADDR "${NET_IF}"
 
-echo "TUNDEV: $TUNDEV"
-echo "WAN IPv4: $NET_ADDR"
-
-# 5. firewall.user バックアップ
-[ -f /etc/firewall.user ] && cp /etc/firewall.user /etc/firewall.user.bak_$(date +%Y%m%d)
-
-# 6. 新しいルール書き込み
-cat > /etc/firewall.user << EOF
-# MAP-E Port Set Expansion (全ポートセット活用)
-
-NET_ADDR="${NET_ADDR}"
-TUNDEV='$TUNDEV'
-PSID=$PSID
-PORT_SET_WIDTH=$PORT_SET_WIDTH
-PORTS_PER_RULE=$PORTS_PER_RULE
-UNITS=$UNITS
-
 rule=0
-while [ \$rule -le 15 ]; do
-    mark=\$(expr \$rule + 16)
-    pn=\$rule
-    portl=\$(expr \$PSID \\* \$PORT_SET_WIDTH + \$rule \\* \$PORTS_PER_RULE)
-    portr=\$(expr \$portl + \$PORTS_PER_RULE - 1)
+while [ $rule -le 15 ]; do
+    mark=$(expr $rule + 16)
+    pn=$rule
+    portl=$(expr $PSID \* $PORT_SET_WIDTH + $rule \* $PORTS_PER_RULE)
+    portr=$(expr $portl + $PORTS_PER_RULE - 1)
 
     # TCPのみをstatisticで分散
-    iptables -t nat -A PREROUTING -p tcp -m statistic --mode nth --every 16 --packet \$pn -j MARK --set-mark \$mark
-    iptables -t nat -A OUTPUT -p tcp -m statistic --mode nth --every 16 --packet \$pn -j MARK --set-mark \$mark
+    iptables -t nat -A PREROUTING -p tcp -m statistic --mode nth --every 16 --packet $pn -j MARK --set-mark $mark
+    iptables -t nat -A OUTPUT -p tcp -m statistic --mode nth --every 16 --packet $pn -j MARK --set-mark $mark
 
     # マークごとのSNAT（TCP）
-    iptables -t nat -A POSTROUTING -p tcp -o \$TUNDEV -m mark --mark \$mark -j SNAT --to \$NET_ADDR:\$portl-\$portr
+    iptables -t nat -A POSTROUTING -p tcp -o $TUNDEV -m mark --mark $mark -j SNAT --to $NET_ADDR:$portl-$portr
 
     # ICMP/UDPは全ポートセットでSNAT
-    iptables -t nat -A POSTROUTING -p icmp -o \$TUNDEV -j SNAT --to \$NET_ADDR:\$portl-\$portr
-    iptables -t nat -A POSTROUTING -p udp -o \$TUNDEV -j SNAT --to \$NET_ADDR:\$portl-\$portr
+    iptables -t nat -A POSTROUTING -p icmp -o $TUNDEV -j SNAT --to $NET_ADDR:$portl-$portr
+    iptables -t nat -A POSTROUTING -p udp -o $TUNDEV -j SNAT --to $NET_ADDR:$portl-$portr
 
-    rule=\$(expr \$rule + 1)
+    rule=$(expr $rule + 1)
 done
 EOF
 
-# 7. ファイアウォール再起動
+# 4. ファイアウォール再起動
 /etc/init.d/firewall restart
 
 echo "設定完了。動作確認をお願いします。"
