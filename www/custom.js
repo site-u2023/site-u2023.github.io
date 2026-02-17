7f1683132321af8298a4a3ee7c63da82831d30a6
site_u <140032047+site-u2023@users.noreply.github.com>
2026-02-17 13:39:11 +0900

    Update custom.js

--- www/custom.js
+++ www/custom.js
@@ -3,7 +3,7 @@ console.log('custom.js (R8.0202.1633) loaded');
 
 // === CONFIGURATION SWITCH ===
 const CONSOLE_MODE = {
-    log: false,   // 通常ログ
+    log: true,   // 通常ログ
     info: false,  // 情報
     warn: false,  // 警告
     debug: false, // デバッグ
