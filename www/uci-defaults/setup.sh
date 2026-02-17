e39a4cea1f92012378494d55ebc9262a3dda1739
site_u <140032047+site-u2023@users.noreply.github.com>
2026-02-17 22:40:13 +0900

    Update setup.sh

--- www/uci-defaults/setup.sh
+++ www/uci-defaults/setup.sh
@@ -22,8 +22,8 @@ RESET() {
 }
 ADDLIST() { uci -q add_list "${SEC}${SEC:+.}$*"; }
 DELLIST() { uci -q del_list "${SEC}${SEC:+.}$*"; }
-LAN="$(GET network.lan.device)"
-WAN="$(GET network.wan.device)"
+LAN="$(GET network.lan.ifname 2>&- || GET network.lan.device 2>&-)"
+WAN="$(GET network.wan.ifname 2>&- || GET network.wan.device 2>&-)"
 ZONE="$(uci show firewall | grep "=zone" | grep "network=.*wan" | cut -d. -f2 | cut -d= -f1 | head -n1)"
 ZONE="${ZONE:-@zone[1]}"
 MEM=$(awk '/MemTotal/{print int($2/1024)}' /proc/meminfo)
